847364be68bcffda1dd52bae075549591e00a052
qwyzex <qwyzex@gmail.com>
2021-12-08 22:08:29 +0700

    starting numberGuessing.py

--- numberGuessing.py
+++ numberGuessing.py
@@ -1,1 +1,29 @@
 import random
+
+low = int(input("Lowest number: "))
+high = int(input("Highest number: "))
+
+num = random.randint(low, high)
+
+print(num)
+
+guess = int(input("Guess: "))
+
+done = False
+while not done:
+    try:
+        if guess == num:
+            print("Win")
+            done = True
+        elif guess > num:
+            print("Too high")
+            guess = int(input("Guess: "))
+        elif guess < num:
+            print("Too low")
+            guess = int(input("Guess: "))
+        else:
+            print("Invalid Input")
+            guess = int(input("Guess: "))
+    except:
+        print("Invalid")
+        guess = int(input("Guess: "))
