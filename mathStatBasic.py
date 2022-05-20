ad0eee1a6d8a98db591cab8dd395b0312c799671
xc17 <xaliscoral@gmail.com>
2022-05-20 20:05:44 +0700

    Math Statistic - Basic

---
+++ mathStatBasic.py
@@ -0,0 +1,49 @@
+from statistics import multimode
+
+raw = input("Array : ")
+
+arr = list(map(int, raw.split(' ')))
+arr.sort()
+
+# calculate
+length = len(arr)
+total = 0
+
+# iterate total
+for n in arr:
+    total += n
+
+even = ''
+
+# define even / odd
+if length % 2 == 0:
+    even = True
+if length % 2 == 1:
+    even = False
+
+mean = total / length
+median = 0
+
+if len(multimode(arr)) == length:
+    modus = "NO MODES"
+elif len(multimode(arr)) == 1:
+    modus = multimode(arr)[0]
+else:
+    modus = multimode(arr)
+
+if even:
+    median = (arr[int(length / 2 - 1)] + arr[int(length / 2)]) / 2
+else:
+    median = arr[int((length + 1) / 2 - 1)]
+
+highest = arr[-1]
+lowest = arr[0]
+
+print('Mean : ', mean)
+print('Median : ', median)
+print('Modus : ', modus, '\n')
+print('Length : ', length)
+print('Total : ', total)
+print('Sorted : ', arr, '\n')
+print('Highest : ', highest)
+print('Lowest : ', lowest)
